204792c400cc724fbdf8afa9514bd071ffac46f0
Mala <malarajupoojari2000@gmail.com>
2024-08-12 14:37:11 +0530

    Implemented winner logic

--- src/App.js
+++ src/App.js
@@ -7,17 +7,58 @@ function Square({value, onSquareClick}){
   )
 }
 
+function calculateWinner(square){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for(let i = 0; i < lines.length ; i++){
+    let [a, b, c] = lines[i];
+    if(square[a] && square[a] === square[b] && square[a] === square[c]){
+      return square[a];
+    }
+  }
+  return null;
+}
+
 function App() {
+  const [isXNext, setIsXNext] = useState(true);
   const [square, setSquare] = useState(Array(9).fill(null));
 
   function handleClick(i){
-    const nextSquare = square.splice();
-    nextSquare[i] = 'X';
+    if(square[i] || calculateWinner(square)){
+      return;
+    }
+    const nextSquare = square.slice();
+    if(isXNext){
+      nextSquare[i] = "X";
+    }
+    else{
+      nextSquare[i] = "O";
+    }
     setSquare(nextSquare);
+    setIsXNext(!isXNext);
+  }
+
+  const winner = calculateWinner(square);
+  let status;
+  if(winner){
+    status = "Winner is: "+ winner;
+  }
+  else{
+    status = "Next player: "+(isXNext ? "X" : "O");
   }
 
   return(
     <>
+      <div className='status'>{status}</div>    
       <div className="board-row">
         <Square value={square[0]} onSquareClick={() => handleClick(0)} />
         <Square value={square[1]} onSquareClick={() => handleClick(1)} />
